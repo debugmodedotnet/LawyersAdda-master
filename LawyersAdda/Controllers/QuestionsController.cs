b749270b3b685a223e11aa402828866ba16d94b0
sourabh somani <sourabh_somani2010@hotmail.com>
2015-12-28 21:50:10 +0530

    28/12/2015-9:46PM

---
+++ LawyersAdda/Controllers/QuestionsController.cs
@@ -0,0 +1,45 @@
+﻿using LawyersAdda.Entities;
+using LawyersAdda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace LawyersAdda.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        //
+        // GET: /Questions/
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AskQuestion()
+        {
+            var lawservices = from r in db.ServiceTypes select r;
+            ViewBag.LawServices = lawservices;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AskQuestion(Question model)
+        {
+            var lawservices = from r in db.ServiceTypes select r;
+            ViewBag.LawServices = lawservices;
+            Random random = new Random();
+            model.QuestionID=random.Next().ToString();
+            model.CreatedDate = DateTime.Now;
+            model.ModifiedDate = DateTime.Now;
+            model.PlainText = model.HTMLText;
+            model.UserID = User.Identity.GetUserId();
+            ApplicationDbContext db1 = new ApplicationDbContext();
+            db.Questions.Add(model);
+            db.SaveChanges();
+            return View();
+        }
+	}
+}
---
+++ LawyersAdda/Entities/Question.cs
@@ -0,0 +1,36 @@
+﻿using LawyersAdda.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LawyersAdda.Entities
+{
+    public class Question
+    {
+        public string QuestionID { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string PlainText { get; set; }
+        [Required]
+        [AllowHtml]
+        [UIHint("tinymce_full_compressed")]
+        [Display(Name = "Page Content")]
+        public string HTMLText { get; set; }
+        public string ServiceID { get; set; }
+
+        [MaxLength(128)]
+        [ForeignKey("User")]
+        public string UserID { get; set; }
+        public bool isAnswered { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
