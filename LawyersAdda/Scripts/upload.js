d09eff7f07a4fdcff6e3ab6d6cc840c4e249e9e9
sourabh somani <sourabh_somani2010@hotmail.com>
2016-02-25 06:40:31 +0530

    25-Feb-2015

--- LawyersAdda/Scripts/upload.js
+++ LawyersAdda/Scripts/upload.js
@@ -8,18 +8,40 @@
     var uploadForm = document.getElementById('js-upload-form');
     var uploadFormFiles = document.getElementById('js-upload-files');
     var uploadedImage = document.getElementById("uploadedImage");
-
     var filesToUpload;
-
+    var UploadedFlag = false;
+    var IncreaseProgress = function () {
+        var valueOfProgress = Number($("#progress").attr("value"));
+        if (valueOfProgress != 100) {
+            $("#progress").attr("value", valueOfProgress + 1);
+            setTimeout(IncreaseProgress, 110);
+        }
+        else {
+            if (UploadedFlag == false) {
+                $("#ProgressHeader").html("Uploaded Successfully...");
+                UploadedFlag = true;
+                setTimeout(IncreaseProgress, 2500);
+            }
+            else {
+                $("#uploadingData").hide();
+                UploadedFlag = false;
+                $("#progress").attr("value", 0);
+                $("#ProgressHeader").html("Uploaded SuccessfullyPlease Wait Photo is Uploading...");
+            }
+        }
+    }
     var startUpload = function(files) {
-        alert("call api to upload");
+        //alert("call api to upload");
+        $("#uploadingData").show();
+        $("#progress").attr("value",0);
+        setTimeout(IncreaseProgress, 300);
         var data = new FormData();
         for (var x = 0; x < files.length; x++) {
             data.append("file" + x, files[x]);
         }
         console.log(data);
-        var progress = document.getElementById("progress");
-        progress.style.display = "inline";
+        //var progress = document.getElementById("progress");
+        //progress.style.display = "inline";
         $.ajax({
             type: "POST",
             url: '/Image/AddImage',
@@ -32,16 +54,19 @@
                 //uploadedImage.innerHTML = uploadedImage.innerHTML + "<a href='" + result + "' class='list-group-item list-group-item-success'><span class='badge alert-success pull-right'>Success</span>" + files[0].name.toString() + "</a>";
                 uploadedImage.innerHTML = uploadedImage.innerHTML + "<div class='col-md-4'><img class='img-thumbnail img-responsive' src='" + result + "' /></div>";
                 uploadedImage.setAttribute("href", result);
-                progress.style.display = "none";
+                //progress.style.display = "none";
+                $("#progress").attr("value", 100);
+                $("#ProgressHeader").html("Uploaded Successfully...");
             },
             error: function (xhr, status, p3, p4){
                 var err = "Error " + " " + status + " " + p3 + " " + p4;
                 if (xhr.responseText && xhr.responseText[0] == "{")
                     err = JSON.parse(xhr.responseText).Message;
                 console.log(err);
+                $("#progress").attr("value", 100);
+                $("#ProgressHeader").html("Error in Uploading...");
             }
         });
-
     }
 
     uploadFormFiles.addEventListener('change', function (e) {
