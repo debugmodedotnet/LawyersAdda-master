c895c79d648e4ed122ebf2ecb5ed05467e2dcb56
sourabh somani <sourabh_somani2010@hotmail.com>
2016-03-11 17:06:23 +0530

    Preloader Added

---
+++ LawyersAdda/Scripts/preloader.js
@@ -0,0 +1,12 @@
+﻿(function($) {
+    "use strict";
+    //Hide Loading Box (Preloader)
+    function handlePreloader() {
+        if($('.preloader').length){
+            $('.preloader').delay(500).fadeOut(500);
+        }
+    }
+    $(window).on('load', function () {
+        handlePreloader();
+    });
+})(window.jQuery);
